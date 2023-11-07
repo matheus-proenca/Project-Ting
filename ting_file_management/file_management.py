859eca67c3ee7fe7219ea355ddc06ee1c968b940
matheus proença <matheusproenca2020@hotmail.com>
2023-11-07 15:11:39 -0300

    Finalizando Req02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith("txt"):
+        sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file) as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
