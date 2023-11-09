91f03f2610a75623beab37750c57019ccffe6ceb
matheus proença <matheusproenca2020@hotmail.com>
2023-11-09 17:10:02 -0300

    Finalizando Req04

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
@@ -12,11 +13,14 @@ def process(path_file, instance):
         if dados["nome_do_arquivo"] == fileDados["nome_do_arquivo"]:
             return None
     instance.enqueue(fileDados)
-    print(fileDados)
+    sys.stdout.write(f"{fileDados}\n")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return sys.stdout.write("Não há elementos\n")
+    path_file = instance.dequeue()["nome_do_arquivo"]
+    print(f"Arquivo {path_file} removido com sucesso")
 
 
 def file_metadata(instance, position):
