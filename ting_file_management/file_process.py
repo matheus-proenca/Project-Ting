296026928f1671d9eee572b3dbfb69905262dada
matheus proença <matheusproenca2020@hotmail.com>
2023-11-09 16:52:04 -0300

    Finalizando Req03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    fileDados = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    for dados in instance._data:
+        if dados["nome_do_arquivo"] == fileDados["nome_do_arquivo"]:
+            return None
+    instance.enqueue(fileDados)
+    print(fileDados)
 
 
 def remove(instance):
